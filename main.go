1bbec813aac3d3edae5d14a33404881634effd6d
Matt Hook <matthew.hook@umajin.com>
2025-05-12 15:26:10 +1200

    4.1.6

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hookenz/gotailwind/v4/downloader"
 )
 
-const TaildwindCssVersion = "v4.1.5"
+const TaildwindCssVersion = "v4.1.6"
 
 func main() {
 	tailwindPath, err := downloader.EnsureTailwindInstalled(TaildwindCssVersion)
